378dccd55e530d9b7fb021b21186db9c71e6ac92
Kennedy Adu <kennedy>
2017-07-26 14:40:05 +0000

    final project

--- motiontest.py
+++ motiontest.py
@@ -1,11 +1,28 @@
-from gpiozero import MotionSensor,LED
+from gpiozero import MotionSensor,LED,Button
 from signal import pause
-
+import urllib2
 pir = MotionSensor(4)
 led = LED(17)
+button=Button(1)
+def iftt():
+	urllib2.urlopen("https://maker.ifttt.com/trigger/motiontest/with/key/g0woufvAOWmL0-vISwBnF7PQ3wMsZ0SNZ1Nblazz-2r")
 
-pir.when_motion = led.on
-pir.when_no_motion = led.off
+#def intruder():
+# print ("intruder")
+
+
+while True:
+	pir.when_motion =led.on
+
+	pir.when_motion = iftt
+#	pir.when_motion=intruder
+#pir.when_no_motion = led.off
+#elif button.when_pressed:
+
+#pir.when_motion=iftt
+
+#Oelse:
+	pir.when_no_motion=led.off
 
 pause()
 
